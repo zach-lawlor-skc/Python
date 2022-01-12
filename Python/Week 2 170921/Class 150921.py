6db4d76a67a8bd64a0ac6670d6e8954a3bff8c02
Zach Lawlor <96421057+zach-lawlor-skc@users.noreply.github.com>
2022-01-12 10:28:42 +0000

    Delete Python directory

--- Python/Week 2 170921/Class 150921.py
+++
@@ -1,8 +0,0 @@
-#personsname= "Zach"
-#FavSubject= input ("Enter your Favourite subject: ")
-#print("Hello", personsname, "You like", FavSubject!)
-#StartingFund = float(input ("Enter your starting funds: "))
-#Wages = 10
-#EndofWeek = StartingFund + Wages
-#print ("You currently have:", EndofWeek)##
-Personsage= input ("Enter your age: ")
--- Python/Week 4 011021/String Commands 270921.py
+++
@@ -1,4 +0,0 @@
-import random
-random2 = (random.randint(6,6))
-random =  (random.randint(1,6))
-print (random + random2)
